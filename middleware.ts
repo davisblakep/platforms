0f720a097393057433f23587f11cd698c8bfe7a0
Blake Davis <blakedavis@Blakes-iMac.local>
2022-09-26 16:35:47 -0400

    Updating middleware file

--- middleware.ts
+++ middleware.ts
@@ -18,10 +18,13 @@ export default function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers.get("host") || "demo.vercel.pub";
+  const hostname = req.headers.get("host") || "platforms-blakedavis.vercel.app";
 
   // Only for demo purposes - remove this if you want to use your root domain as the landing page
-  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
+  if (
+    hostname === "platforms-blakedavis.vercel.app" ||
+    hostname === "platforms.vercel.app"
+  ) {
     return NextResponse.redirect("https://demo.vercel.pub");
   }
 
@@ -32,7 +35,7 @@ export default function middleware(req: NextRequest) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.pub`, "")
+          .replace(`.platforms-blakedavis.vercel.app`, "")
           .replace(`.platformize.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
   // rewrites for app pages
